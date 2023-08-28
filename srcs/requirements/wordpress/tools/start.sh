2cfc4b956705439627db36cd381280d8475c5c7f
willaCS <arthur.willems.prive@gmail.com>
2023-08-28 14:53:29 +0000

    updated script

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,50 +1,48 @@
 #!/bin/sh
 
-# exit_if_fail(){
-# 	if [ $? -gt 0 ]; then
-# 		echo "[ERROR]"
-# 		exit 1;
-# 	fi
-# }
+exit_if_fail() {
+    if [ $? -gt 0 ]; then
+        echo "[ERROR] $1"
+        exit 1
+    fi
+}
 
-# check_db () {
-# 	wp db check
-# }
+check_db () {
+	wp db check
+	exit_if_fail "db connection faild!"
+}
 
-# run_install () {
-# 	echo "[INFO] Create wordpress config"
-# 	wp config create \
-# 		--dbname=$SQL_DATABASE \
-# 		--dbuser=$SQL_USER \
-# 		--dbpass=$SQL_PASSWORD \
-# 		--dbhost=$SQL_HOST
-# 	exit_if_fail;
+run_install () {
+	echo "[INFO] Create wordpress config"
+	wp config create \
+		--dbname=$SQL_DATABASE \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=$SQL_HOST
 	
-# 	check_db;
-# 	exit_if_fail;
+	check_db;
 
-# 	echo "[INFO] Install and create admin user"
-# 	wp core install \
-# 		--url=$WP_URL \
-# 		--title="$WP_TITLE" \
-# 		--admin_user=$WP_ADMIN_NAME \
-# 		--admin_password=$WP_ADMIN_PASSWD \
-# 		--admin_email=$WP_ADMIN_EMAIL
-# 	exit_if_fail;
+	echo "[INFO] Install and create admin user"
+	wp core install \
+		--url=$WP_URL \
+		--title="$WP_TITLE" \
+		--admin_user=$WP_ADMIN_NAME \
+		--admin_password=$WP_ADMIN_PASSWD \
+		--admin_email=$WP_ADMIN_EMAIL
+	exit_if_fail "WP install failed!";
 
-# 	echo "[INFO] Create $WP_USER_NAME user"
-# 	wp user create $WP_USER_NAME $WP_USER_EMAIL \
-# 		--user_pass=$WP_USER_PASSWD \
-# 		--display_name=$WP_USER_DISPLAY_NAME 
-# 	exit_if_fail;
-# }
+	echo "[INFO] Create $WP_USER_NAME user"
+	wp user create $WP_USER_NAME $WP_USER_EMAIL \
+		--user_pass=$WP_USER_PASSWD \
+		--display_name=$WP_USER_DISPLAY_NAME 
+	exit_if_fail "Faild to create $WP_USER_NAME!";
+}
 
-# wp config path 2>/dev/null
-
-# if [ $? -eq 0 ]; then
-# 	check_db;
-# else
-# 	run_install;
-# fi
+wp config path
+if [ $? -eq 0 ]; then
+	check_db;
+else
+	run_install;
+fi
 
 php-fpm7.4 -F
