0d606267f460f04bc3b715646ed3c11f54eca9bf
willaCS <arthur.willems.prive@gmail.com>
2023-08-28 09:26:33 +0000

    added ping

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -6,4 +6,5 @@ cat /tmp/query.sql \
 | sed "s/SQL_PASSWORD/$SQL_PASSWORD/g" \
 > /tmp/query.sql
 
+sed -i 's/bind-address\s*= *[^ ]*/bind-address = 0.0.0.0\nport = 3306/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 mysqld_safe --init-file=/tmp/query.sql
